f93f00fe3363c5b60d3c74e9555966c2cd583628
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-01-19 15:13:03 +0100

    work in progress

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,24 @@
-//  Add your code here
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const celebritySchema = new Schema ({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    catchPhrase: {
+        type: String,
+        required: true,
+    }
+},
+{
+    timestamps: true
+})
+
+const Celebrity = mongoose.model('Celebrity', celebritySchema);
+module.exports = Celebrity;
